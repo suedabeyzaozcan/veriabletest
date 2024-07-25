24a9f9fe3bd0bd1d3ddee09493cdb95cf5379c9f
Sueda Beyza Özcan <104599567+suedabeyzaozcan@users.noreply.github.com>
2024-07-25 15:51:47 +0300

    Add files via upload

---
+++ VariableTest/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  VariableTest
+//
+//  Created by Sueda Beyza Özcan on 19.07.2024.
+//
+
+import Foundation
+
+print("Hello, World!")
+var MyNumber=5*10
+print(MyNumber)
+
+
+
+
